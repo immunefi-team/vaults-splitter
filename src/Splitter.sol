1b88e0a259c577bdbb7b74bfdd199c17c46326d6
goncaloMagalhaes <goncalo@immunefi.com>
2023-06-14 14:42:47 +0100

    fix(splitter): fix some minor bugs and add invariant testing

--- src/Splitter.sol
+++ src/Splitter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.17;
+pragma solidity 0.8.18;
 
 import { IERC20 } from "openzeppelin-contracts/interfaces/IERC20.sol";
 import { SafeERC20 } from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
@@ -33,25 +33,29 @@ contract Splitter is Ownable, Withdrawable, ReentrancyGuard {
 
     uint256 public fee;
     uint256 public constant FEE_BASIS = 100_00;
-    uint256 public immutable MAX_FEE;
+    uint256 public immutable maxFee;
+    uint256 public immutable gasCap;
     address payable public feeRecipient;
 
-    constructor(uint256 maxFee, uint256 _fee, address _owner, address payable _feeRecipient) {
-        require(maxFee <= FEE_BASIS, "Splitter: MAX_FEE must be below FEE_BASIS");
-        MAX_FEE = maxFee;
+    constructor(uint256 _gasCap, uint256 _maxFee, uint256 _fee, address _owner, address payable _feeRecipient) {
+        require(_maxFee <= FEE_BASIS, "Splitter: maxFee must be below FEE_BASIS");
+        maxFee = _maxFee;
+
+        gasCap = _gasCap;
 
         _setFee(_fee);
 
-        feeRecipient = _feeRecipient;
+        _changeFeeRecipient(_feeRecipient);
+
         _transferOwnership(_owner);
     }
 
     /**
      * @notice internal set fee
-     * @param newFee value of the new fee. Must be less than MAX_FEE
+     * @param newFee value of the new fee. Must be less than maxFee
      */
     function _setFee(uint256 newFee) internal {
-        require(newFee <= MAX_FEE, "Splitter: fee must be below MAX_FEE");
+        require(newFee <= maxFee, "Splitter: fee must be below maxFee");
 
         emit FeeChanged(fee, newFee);
         fee = newFee;
@@ -71,6 +75,8 @@ contract Splitter is Ownable, Withdrawable, ReentrancyGuard {
      * @param newFeeRecipient address of new fee recipient
      */
     function _changeFeeRecipient(address payable newFeeRecipient) internal {
+        require(newFeeRecipient != address(0), "Splitter: FeeRecipient cannot be null address");
+
         emit FeeRecipientChanged(feeRecipient, newFeeRecipient);
 
         feeRecipient = newFeeRecipient;
@@ -103,36 +109,67 @@ contract Splitter is Ownable, Withdrawable, ReentrancyGuard {
         uint256 nativeTokenAmt,
         uint256 gas
     ) public payable nonReentrant {
-        for (uint256 i; i < payout.length; i++) {
+        require(wh != address(0), "Splitter: Whitehat address cannot be null");
+        require(gas <= gasCap, "Splitter: Gas greater than max allowed");
+
+        for (uint256 i = 0; i < payout.length; i++) {
             uint256 feeAmount = (payout[i].amount * fee) / FEE_BASIS;
             if (feeAmount > 0) IERC20(payout[i].token).safeTransferFrom(msg.sender, feeRecipient, feeAmount);
             IERC20(payout[i].token).safeTransferFrom(msg.sender, wh, payout[i].amount);
         }
 
         if (nativeTokenAmt > 0) {
-            uint256 feeAmount = (nativeTokenAmt * fee) / FEE_BASIS;
-            if (feeAmount > 0) {
-                (bool successFee, ) = feeRecipient.call{ value: feeAmount }("");
-                require(successFee, "Splitter: Failed to send ether to fee receiver");
-            }
-            (bool successWh, ) = wh.call{ value: nativeTokenAmt, gas: gas }("");
-            require(successWh, "Splitter: Failed to send ether to whitehat");
-
-            uint256 nativeAmountDistributed = nativeTokenAmt + feeAmount;
-            if (msg.value > nativeAmountDistributed) {
-                (bool successRefund, ) = msg.sender.call{ value: (msg.value - nativeAmountDistributed) }("");
-                require(successRefund, "Splitter: Failed to refund to msg.sender");
-            }
+            _payWhitehatNative(wh, nativeTokenAmt, gas);
+        } else if (msg.value > 0) {
+            // must refund msg.sender
+            _refundCaller(msg.value);
         }
 
-        emit PayWhitehat(msg.sender, referenceId, wh, payout, nativeTokenAmt, feeRecipient, fee);
+        if (nativeTokenAmt > 0 || payout.length > 0) {
+            emit PayWhitehat(msg.sender, referenceId, wh, payout, nativeTokenAmt, feeRecipient, fee);
+        }
+    }
+
+    /**
+     * @notice Pays whitehat in native token
+     * @param wh whitehat address
+     * @param nativeTokenAmt The payout of native Ether amount to whitehat
+     * @param gas The amount of gas to forward to the whitehat to mitigate gas griefing
+     */
+    function _payWhitehatNative(address payable wh, uint256 nativeTokenAmt, uint256 gas) internal {
+        uint256 feeAmount = (nativeTokenAmt * fee) / FEE_BASIS;
+        if (feeAmount > 0) {
+            // feeRecipient is trusted, we can skip this check
+            // slither-disable-next-line arbitrary-send-eth,low-level-calls
+            (bool successFee, ) = feeRecipient.call{ value: feeAmount }("");
+            require(successFee, "Splitter: Failed to send ether to fee receiver");
+        }
+        // wh is not trusted but, we cap execution and avoid to re-enter, we can skip this check
+        // slither-disable-next-line arbitrary-send-eth,low-level-calls
+        (bool successWh, ) = wh.call{ value: nativeTokenAmt, gas: gas }("");
+        require(successWh, "Splitter: Failed to send ether to whitehat");
+
+        uint256 nativeAmountDistributed = nativeTokenAmt + feeAmount;
+        if (msg.value > nativeAmountDistributed) {
+            _refundCaller(msg.value - nativeAmountDistributed);
+        }
+    }
+
+    /**
+     * @notice Refunds msg.sender
+     * @param amount Amount to refund
+     */
+    function _refundCaller(uint256 amount) internal {
+        // slither-disable-next-line arbitrary-send-eth,low-level-calls
+        (bool success, ) = msg.sender.call{ value: amount }("");
+        require(success, "Splitter: Failed to refund to msg.sender");
     }
 
     /**
      * @notice Withdraw asset ERC20 or ETH
-     * @param _assetAddress Asset to be withdrawn.
+     * @param assetAddress Asset to be withdrawn.
      */
-    function withdrawERC20ETH(address _assetAddress) public virtual override onlyOwner {
-        super.withdrawERC20ETH(_assetAddress);
+    function withdrawERC20ETH(address assetAddress) public override onlyOwner {
+        super.withdrawERC20ETH(assetAddress);
     }
 }
---
+++ test/foundry/Splitter.invariants.t.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+import { Test } from "forge-std/Test.sol";
+import { Splitter } from "../../src/Splitter.sol";
+import { Handler } from "./handlers/Handler.sol";
+
+contract SplitterInvariantsTest is Test {
+    // solhint-disable state-visibility
+    address owner;
+    address payable feeRecipient;
+    uint256 gasCap;
+
+    Splitter splitter;
+    Handler handler;
+
+    function setUp() public {
+        owner = vm.addr(1);
+        feeRecipient = payable(vm.addr(2));
+        gasCap = 500_000;
+
+        splitter = new Splitter(gasCap, 1_500, 1_000, owner, feeRecipient);
+
+        handler = new Handler(splitter);
+
+        // we specify selectors and target contract to be fuzzed
+        bytes4[] memory selectors = new bytes4[](1);
+        selectors[0] = Handler.payWhitehat.selector;
+
+        targetSelector(FuzzSelector({ addr: address(handler), selectors: selectors }));
+        targetContract(address(handler));
+    }
+
+    /**
+     @dev invariant: should not have any leftover funds from payments
+     * It should be noted that the invariant is not that the splitter cannot have stucked funds, 
+     * but that the splitter cannot have stucked funds due to payments. 
+     * We acknowledge anybody can send funds to the splitter.
+     */
+    function invariant_noLeftoverFundsFromPayments() public {
+        assertEq(address(splitter).balance, 0);
+        assertEq(handler.token().balanceOf(address(splitter)), 0);
+    }
+
+    // add this to be excluded from coverage report
+    function test() public {}
+}
